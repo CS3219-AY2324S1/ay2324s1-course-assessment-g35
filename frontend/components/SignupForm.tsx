6d69aca0880bcc24d1adfc47dccd9ddfc34f8c42
kennethsim2000 <e0725254@u.nus.edu>
2023-09-10 22:27:10 +0800

    Complete login and register page

---
+++ frontend/components/SignupForm.tsx
@@ -0,0 +1,134 @@
+import axios from "axios";
+import { useState } from "react";
+
+const SignupForm: React.FC = ({}) => {
+  const [role, setRole] = useState<string>("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRoleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRole(e.target.value);
+  };
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const body = {
+        username: username,
+        password: password,
+        role: role,
+      };
+      console.log(body);
+      const response = await axios.post("http://localhost:8000/register", body);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="bg-gray-50 ">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md  ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              Create an account
+            </h1>
+            <form className="space-y-4 md:space-y-6">
+              <div>
+                <label
+                  htmlFor="Username"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Username
+                </label>
+                <input
+                  type="Username"
+                  name="Username"
+                  id="Username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                  className="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5 "
+                  placeholder="john123"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  placeholder="••••••••"
+                  className="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 "
+                  required
+                />
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Role
+                </label>
+                <div>
+                  <label className="inline-flex items-center">
+                    <input
+                      type="radio"
+                      name="role"
+                      value="user"
+                      checked={role === "user"}
+                      onChange={handleRoleChange}
+                      className="form-radio h-4 w-4 text-blue-600"
+                    />
+                    <span className="ml-2">User</span>
+                  </label>
+                  <label className="inline-flex items-center ml-6">
+                    <input
+                      type="radio"
+                      name="role"
+                      value="admin"
+                      checked={role === "admin"}
+                      onChange={handleRoleChange}
+                      className="form-radio h-4 w-4 text-blue-600"
+                    />
+                    <span className="ml-2">Admin</span>
+                  </label>
+                </div>
+              </div>
+
+              <button
+                onClick={handleSubmit}
+                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              >
+                Create an account
+              </button>
+              <p className="text-sm font-light text-gray-500 ">
+                Already have an account?{" "}
+                <a
+                  href="/Login"
+                  className="font-medium text-primary-600 hover:underline "
+                >
+                  Login here
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
---
+++ frontend/pages/Login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/LoginForm";
+import React from "react";
+
+export default function userLogin() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen w-screen">
+        <LoginForm />
+      </div>
+    </>
+  );
+}
