247799db0c630434a46bfa4b627553c7f20311b8
yuehernkang <yuehernkang@hotmail.com>
2023-09-11 14:58:49 +0800

    Added delete user functionality

--- user-service/src/service.ts
+++ user-service/src/service.ts
@@ -1,17 +1,24 @@
+import { User } from "@prisma/client";
 import db from "./utils/db.server";
 
 type UserDto = {
-    id: string;
-    username: string;
-}
+  id: string;
+  username: string;
+};
 
 export const getAllUsers = async (): Promise<UserDto[]> => {
-    return await db.user.findMany(
-        {
-            select: {
-                id: true,
-                username: true
-            }
-        }
-    );
-}
+  return await db.user.findMany({
+    select: {
+      id: true,
+      username: true,
+    },
+  });
+};
+
+export const deleteUser = async (userid: string): Promise<User> => {
+  return await db.user.delete({
+    where: {
+      id: userid,
+    },
+  });
+};
