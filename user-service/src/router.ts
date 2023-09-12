03357180c40c256786544d806fbfb4224c289065
Timothy Law <e0725564@u.nus.edu>
2023-09-12 23:37:24 +0800

    feat: add createUser route

--- user-service/src/router.ts
+++ user-service/src/router.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, getAllUsers } from "./service";
+import { createUser, deleteUser, getAllUsers } from "./service";
 
 export const userRouter = express.Router();
 
@@ -8,6 +8,15 @@ userRouter.get("/", async (req, res) => {
   try {
     const users = await getAllUsers();
     return res.status(200).json(users);
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
+
+userRouter.post("/", async (req, res) => {
+  try {
+    const user = await createUser(req.body);
+    return res.status(201).json(user);
   } catch (error: any) {
     return res.status(500).json(error.message);
   }
