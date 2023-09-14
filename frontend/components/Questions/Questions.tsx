88538e9207416004b3b51982f737b97eaebd7510
tangyuantyy <e0774642@u.nus.edu>
2023-09-14 20:35:01 +0800

    implement design for assignment 1 page

---
+++ frontend/components/Questions/Questions.tsx
@@ -0,0 +1,45 @@
+import { AddIcon } from '@/icons/icons';
+import QuestionField from './QuestionField';
+import styles from './Questions.module.css';
+
+export default function Questions() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.title}>
+        <div className={styles.header}>
+          Practice Questions
+        </div>
+        <AddIcon />
+        </div>
+        <div className={styles.table}>
+            <div className={styles['table-header']}>
+                <div className={styles.section}>
+                id
+                </div>
+                <div className={styles['md-section']}>
+                Title
+                </div>
+                <div className={styles['lg-section']}>
+                Description
+                </div>
+                <div className={styles.section}>
+                Category
+                </div>
+                <div className={styles.section}>
+                Complexity
+                </div>
+            </div>
+            <div className={styles.line} />
+            <div>
+                <QuestionField
+                id={1}
+                title={'123'}
+                description={'123'}
+                category={'123'}
+                complexity={'123'}
+                />
+            </div>
+        </div>
+    </div>
+  );
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Questions from "@/components/Questions/Questions";
+import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="main">
+       <Header />
+       <Questions />
+    </div>
   );
 }
