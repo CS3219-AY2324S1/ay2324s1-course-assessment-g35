05a83106f75836342cee85bfa5bb919146aeafb3
Timothy Law <e0725564@u.nus.edu>
2023-09-17 15:42:35 +0800

    feat: add routing

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { Inter } from "@next/font/google";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     const token = localStorage.getItem("token");
@@ -27,6 +29,12 @@ export default function Home() {
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button onClick={handleSubmit}>Click me</button>
+      <br />
+      <button onClick={() => localStorage.removeItem("token")}>
+        Remove token
+      </button>
+      <br />
+      <button onClick={() => router.push("/Profile")}>Profile</button>
     </>
   );
 }
