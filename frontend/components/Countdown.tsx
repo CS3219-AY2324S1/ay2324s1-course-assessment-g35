fadd774371c72732a056d94ee14aa75656bec4c8
Timothy Law <e0725564@u.nus.edu>
2023-10-02 01:23:00 +0800

    feat: Add Countdown to match button

---
+++ frontend/components/Countdown.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+
+interface CountdownProps {
+  seconds: number;
+  isRunning: boolean;
+  onTimerEnd: () => void;
+}
+
+const Countdown: React.FC<CountdownProps> = ({ seconds, isRunning, onTimerEnd }) => {
+    const [count, setCount] = useState(seconds);
+
+    useEffect(() => {
+        let intervalId: NodeJS.Timeout | undefined;
+
+        if (isRunning) {
+            intervalId = setInterval(() => {
+                setCount((prevCount) => {
+                    if (prevCount > 1) { // decrement every second
+                        return prevCount - 1;
+                    } else { // clear interval when count is 0
+                        clearInterval(intervalId);
+                        onTimerEnd();
+                        return 0;
+                    }
+                });
+            }, 1000);
+        } else if (intervalId) { // clear interval when isRunning is false
+            clearInterval(intervalId);
+        }
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [isRunning]);
+
+    return (
+        <div>
+            <h2>[{count}]</h2>
+        </div>
+    );
+};
+
+export default Countdown;
