da9f5eba8059703e35b08b1b78af7d0d949a85cd
yuehernkang <yuehernkang@hotmail.com>
2023-10-19 03:12:19 +0800

    History service

---
+++ history-service/src/router.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { findHistoryByUserId } from "./service";
+
+export const historyRouter = express.Router();
+
+historyRouter.get("/", async (req, res) => {
+  try {
+    const userId = typeof req.query.userId === "string" ? req.query.userId : "";
+    const history = await findHistoryByUserId(userId);
+    return res.status(200).json(history);
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
