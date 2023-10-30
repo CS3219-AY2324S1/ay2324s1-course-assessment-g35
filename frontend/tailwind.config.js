b4bc871b19e5cee0957a1a59b651e098f3e9a17d
cetigerlily <92414500+cetigerlily@users.noreply.github.com>
2023-10-30 19:04:29 +0800

    Add in PeerPrep colors for Tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pp-red': '#FF1053',
+        'pp-blue': '#88D9E6',
+        'pp-gray': '#2E2A36',
+        'pp-darkpurple': '#190B28',
+        'pp-lightpurple': '#6C6EA0',
+      },
+    },
   },
   plugins: [],
 };
