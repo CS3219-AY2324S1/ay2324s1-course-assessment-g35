100715962fe2578eab780be5fd26622d87783c54
yuehern <1600084f@student.tp.edu.sg>
2023-11-04 16:44:40 +0800

    Formatting of code examples

--- frontend/code_examples/codeExamples.ts
+++ frontend/code_examples/codeExamples.ts
@@ -1,41 +1,34 @@
 export const codeExamples: { [key: string]: string } = {
-  javascript: `
-    console.log('Hello, World!');
+  javascript: `console.log('Hello, World!');
         `,
-  cpp: `
-    #include <iostream>
-    int main() {
-        std::cout << "Hello, World!" << std::endl;
-        return 0;
+  cpp: `#include <iostream>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+        `,
+  java: `public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
+}
         `,
-  java: `
-    public class HelloWorld {
-        public static void main(String[] args) {
-            System.out.println("Hello, World!");
-        }
-    }
+  go: `package main
+import "fmt"
+func main() {
+    fmt.Println("Hello, World!")
+}
         `,
-  go: `
-    package main
-    import "fmt"
-    func main() {
-        fmt.Println("Hello, World!")
-    }
+  python: `print("Hello, World!")
         `,
-  python: `
-    print("Hello, World!")
+  kotlin: `fun main() {
+    println("Hello, World!")
+}
         `,
-  kotlin: `
-    fun main() {
-        println("Hello, World!")
-    }
-        `,
-  c: `
-    #include <stdio.h>
-    int main() {
-        printf("Hello, World!\n");
-        return 0;
-    }
+  c: `#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
         `,
 };
