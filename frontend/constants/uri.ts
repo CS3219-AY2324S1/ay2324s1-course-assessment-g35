f5de514caedec08b1c9ed66a26ccca4395b0b5e2
kennethsim2000 <e0725254@u.nus.edu>
2023-11-09 19:49:51 +0800

    Add try catch and chat deployment

--- frontend/constants/uri.ts
+++ frontend/constants/uri.ts
@@ -21,7 +21,10 @@ export const QUESTION_URI = {
 
 export const MATCHINGSERVICE_URI = 'http://localhost:3001';
 
-export const CHATSERVICE_URI = 'http://localhost:3002';
+// export const CHATSERVICE_URI = 'http://localhost:3002';
+export const CHATSERVICE_URI = 'http://35.197.156.185:3002';
+
+
 
 const HISTORYSERVICE_URI = 'http://localhost:8002';
 
