9bd6d7f843a7f9cf2eb8993b55661538f4ea6dd2
yuehern <1600084f@student.tp.edu.sg>
2023-11-11 16:44:46 +0800

    Revert "Modify jenkinfile"
    This reverts commit 1f07bf96034da548ba8a517b9078b30d105a5b58.

--- frontend/credentials.ts
+++ frontend/credentials.ts
@@ -7,11 +7,3 @@ export const firebaseConfig = {
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId,
 };
-echo 'export API_KEY="AIzaSyDX8gLnFkXmqZgAyxelm_Oe8nBuVEVdI8Q"' >> ~/.bashrc
-echo 'export AUTH_DOMAIN="astral-shape-402017.firebaseapp.com"' >> ~/.bashrc
-echo 'export DATABASE_URL="https://astral-shape-402017-default-rtdb.asia-southeast1.firebasedatabase.app"' >> ~/.bashrc
-echo 'export PROJECT_ID="astral-shape-402017"' >> ~/.bashrc
-echo 'export STORAGE_BUCKET="astral-shape-402017.appspot.com"' >> ~/.bashrc
-echo 'export MESSAGING_SENDER_ID="508111642693"' >> ~/.bashrc
-echo 'export APP_ID="1:508111642693:web:5052057c67f03236b6832c"' >> ~/.bashrc
-source ~/.bashrc
