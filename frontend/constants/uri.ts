7c8fed5ffcdfe5d21449f4f91c4f20a5cca66a13
yuehern <1600084f@student.tp.edu.sg>
2023-11-14 16:24:31 +0800

    modified urls

--- frontend/constants/uri.ts
+++ frontend/constants/uri.ts
@@ -1,34 +1,34 @@
 // constants for api uris
 
-const USERSERVICE_URI = process.env.NEXT_PUBLIC_USERSERVICE_URI || 'http://localhost:8000';
+const USERSERVICE_URI = "34.126.107.52:8000";
 
 export const USER_URI = {
-    LOGIN: `${USERSERVICE_URI}/login`,
-    REGISTER: `${USERSERVICE_URI}`,
-    LOGOUT: `${USERSERVICE_URI}/logout`,
-    EDIT: `${USERSERVICE_URI}/edit`,
-    DELETE: `${USERSERVICE_URI}`,
-    GET_TOKEN: `${USERSERVICE_URI}`,
-    VERIFY_TOKEN: `${USERSERVICE_URI}/verify`,
-    RETRIEVE:`${USERSERVICE_URI}/retrieve`
+  LOGIN: `${USERSERVICE_URI}/login`,
+  REGISTER: `${USERSERVICE_URI}`,
+  LOGOUT: `${USERSERVICE_URI}/logout`,
+  EDIT: `${USERSERVICE_URI}/edit`,
+  DELETE: `${USERSERVICE_URI}`,
+  GET_TOKEN: `${USERSERVICE_URI}`,
+  VERIFY_TOKEN: `${USERSERVICE_URI}/verify`,
+  RETRIEVE: `${USERSERVICE_URI}/retrieve`,
 };
 
-const QUESTIONSERVICE_URI = process.env.NEXT_PUBLIC_QUESTIONSERVICE_URI || 'http://localhost:8001';
+const QUESTIONSERVICE_URI = "34.142.159.230:8000";
 
 export const QUESTION_URI = {
-    GET_RANDOM_QUESTION: `${QUESTIONSERVICE_URI}/random-difficulty`,
-    GET_BY_ID: `${QUESTIONSERVICE_URI}/id`,
+  GET_RANDOM_QUESTION: `${QUESTIONSERVICE_URI}/random-difficulty`,
+  GET_BY_ID: `${QUESTIONSERVICE_URI}/id`,
 };
 
-export const MATCHINGSERVICE_URI = process.env.NEXT_PUBLIC_MATCHINGSERVICE_URI || 'http://localhost:3001';
+export const MATCHINGSERVICE_URI = "http://34.87.167.153:3001";
 
 // export const CHATSERVICE_URI = 'http://localhost:3002';
-export const CHATSERVICE_URI = process.env.NEXT_PUBLIC_CHATSERVICE_URI || 'http://localhost:3002';
+export const CHATSERVICE_URI = "http://34.87.120.71:3002";
 
-const HISTORYSERVICE_URI = process.env.NEXT_PUBLIC_HISTORYSERVICE_URI || 'http://localhost:8002';
+const HISTORYSERVICE_URI = "http://34.126.71.94:8000";
 
 export const HISTORY_URI = {
-    GET_ALL: `${HISTORYSERVICE_URI}/all`,
-    GET_BY_USERNAME: `${HISTORYSERVICE_URI}/getUserHistory`,
-    CREATE_OR_UPDATE: `${HISTORYSERVICE_URI}/create`,
+  GET_ALL: `${HISTORYSERVICE_URI}/all`,
+  GET_BY_USERNAME: `${HISTORYSERVICE_URI}/getUserHistory`,
+  CREATE_OR_UPDATE: `${HISTORYSERVICE_URI}/create`,
 };
